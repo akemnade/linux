48444544a222c82e8b504c043ede6f9f230fc8b6
Andreas Kemnade <andreas@kemnade.info>
2020-12-12 16:24:04 +0100

    drm: Add skeleton for EPDC driver
    This driver is for the EPD controller in the i.MX SoCs.
    
    Signed-off-by: Andreas Kemnade <andreas@kemnade.info>

---
+++ drivers/gpu/drm/mxc-epdc/mxc_epdc.h
@@ -0,0 +1,20 @@
+/* SPDX-License-Identifier: GPL-2.0+ */
+/* Copyright (C) 2020 Andreas Kemnade */
+
+#include <video/display_timing.h>
+#include <video/of_display_timing.h>
+#include <video/videomode.h>
+
+#include <drm/drm_drv.h>
+#include <drm/drm_connector.h>
+#include <drm/drm_simple_kms_helper.h>
+
+struct clk;
+struct regulator;
+struct mxc_epdc {
+	struct drm_device drm;
+	struct drm_simple_display_pipe pipe;
+	struct drm_connector connector;
+	struct display_timing timing;
+};
+
