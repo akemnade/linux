24762ed56e18568839865d61d99540261f897464
Andreas Kemnade <andreas@kemnade.info>
2023-06-13 19:39:48 +0200

    test waveform lower 4bit

--- drivers/gpu/drm/mxc-epdc/epdc_update.c
+++ drivers/gpu/drm/mxc-epdc/epdc_update.c
@@ -848,7 +848,7 @@ int mxc_epdc_send_single_update(struct drm_rect *clip, int pitch, void *vaddr,
 {
 	struct update_desc_list *upd_desc;
 
-	if (priv->rev < 30) 
+	if (1) 
 		epdc_from_rgb_clear_lower_nibble(clip, vaddr, pitch,
 						 (u8 *)priv->epdc_mem_virt,
 						 priv->epdc_mem_width);
